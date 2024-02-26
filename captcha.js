5b9f62f7ed2ba757bbd8ab861158cccd4b9b2922
asstrooo <127354535+asstrooo@users.noreply.github.com>
2024-02-27 00:33:24 +0100

    Create captcha.js

---
+++ captcha.js
@@ -0,0 +1,37 @@
+function getRandomNumber(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  }
+  
+  
+  function generateCaptcha() {
+    const num1 = getRandomNumber(1, 10);
+    const num2 = getRandomNumber(1, 10);
+    const operator = ['+', '-', '*'][getRandomNumber(0, 2)];
+    const expression = `${num1} ${operator} ${num2}`;
+    const result = eval(expression);
+    return { expression, result };
+  }
+  
+  
+  function displayCaptcha() {
+    const captcha = generateCaptcha();
+    document.getElementById('captcha').textContent = captcha.expression;
+    sessionStorage.setItem('captchaResult', captcha.result);
+  }
+  
+  /a
+  function validateCaptcha() {
+    const userInput = document.getElementById('captchaInput').value;
+    const captchaResult = sessionStorage.getItem('captchaResult');
+    
+    if (userInput === captchaResult) {
+      alert('Captcha válido!');
+     
+    } else {
+      alert('Captcha inválido. Por favor, inténtalo de nuevo.');
+      displayCaptcha(); 
+    }
+  }
+  
+  
+  window.onload = displayCaptcha;
